4dbfaba93f9d83457f13c7456f2126fd80ecc65f
Mumuuuuu2 <aube.lee@twolinecode.com>
2022-08-18 16:41:45 +0900

    sorc

--- basic_board/src/components/LoginForm.vue
+++ basic_board/src/components/LoginForm.vue
@@ -1,37 +1,24 @@
 <template>
-
   <!-- MODAL popup layout -->
   <div class="black-bg" v-if="joinStatus">
     <div class="white-bg">
-      <h2>JOIN US</h2>
-      <ul>
-        <li>
-          email <input type="email" v-model="email" />
-        </li>
-        <li>
-          name <input v-model="name" />
-        </li>
-        <li>
-          password <input type="password" />
-        </li>
-        <li>
-          password check <input type="password" />
-        </li>
-        <li>
-          {{passwordCheck}} <v-btn small @click="onCheck">check</v-btn>
-        </li>
-        <li>
-          city <input v-model="city" />
-        </li>
-        <li>
-          street <input v-model="street" />
-        </li>
-        <li>
-          post code <input v-model="zipcode" />
-        </li>
-      </ul>
-      <v-btn large @click="joinStatus = false" >CANCEL</v-btn>
-      <v-btn large @click="onClickJoin" >SUBMIT</v-btn>
+      <div style="width: 80%;">
+        <h2>JOIN US</h2>
+        <ul>
+          <li>email <input type="email" v-model="email" /></li>
+          <li>name <input v-model="name" /></li>
+          <li>password <input type="password" /></li>
+          <li>password check <input type="password" /></li>
+          <li>
+            {{ passwordCheck }} <v-btn small @click="onCheck">check</v-btn>
+          </li>
+          <li>city <input v-model="city" /></li>
+          <li>street <input v-model="street" /></li>
+          <li>post code <input v-model="zipcode" /></li>
+        </ul>
+        <v-btn large @click="joinStatus = false" class="vBtn">CANCEL</v-btn>
+        <v-btn large @click="onClickJoin" class="vBtn">SUBMIT</v-btn>
+      </div>
     </div>
   </div>
 
@@ -40,34 +27,33 @@
     <div>
       <h1>{{ msg }}</h1>
       <div>
-        <ul style="text-align: center;">
+        <ul style="text-align: center">
           <li>
-          <input  type="email" v-model="email" placeholder="ID(E-mail)" />
+            <input type="email" v-model="email" placeholder="ID(E-mail)" />
           </li>
           <li>
-          <input type="password" v-model="password" placeholder="Password" />
+            <input type="password" v-model="password" placeholder="Password" />
           </li>
         </ul>
-        <v-btn large @click="onClickLogin" >GO</v-btn>
-        <v-btn large @click="joinStatus = true" >Join us</v-btn>
+        <v-btn large @click="onClickLogin" class="vBtn">GO</v-btn>
+        <v-btn large @click="joinStatus = true" class="vBtn">Join us</v-btn>
       </div>
     </div>
   </div>
-
 </template>
 
 <script>
 export default {
-  name: 'LoginForm',
+  name: "LoginForm",
   data: () => ({
     joinStatus: false,
-    passwordCheck: '',
-    email: '',
-    password: '',
-    name: '',
-    city: '',
-    street: '',
-    zipcode: ''
+    passwordCheck: "",
+    email: "",
+    password: "",
+    name: "",
+    city: "",
+    street: "",
+    zipcode: "",
   }),
   methods: {
     // 로그인 api 실행
@@ -75,33 +61,31 @@ export default {
       //console.log("onClickLogin press");
       console.log(this.$data);
       // const URL = '/auth/login';
-      const URL = 'https://jssampletest.herokuapp.com/api/auth/login';
+      const URL = "https://jssampletest.herokuapp.com/api/auth/login";
 
       // let callUrl = apiBaseUrl + URL;
       // console.log(callUrl);
-      this.$axios.get(URL,
-        [{
+      this.$axios.get(URL, [
+        {
           headers: {
-            'accept': '*/*',
-            'Content-Type': 'application/json'
+            accept: "*/*",
+            "Content-Type": "application/json",
           },
-          data: this.$data
-        }]
-      )
+          data: this.$data,
+        },
+      ]);
     },
     // 회원가입 data 전달
     onClickJoin() {
       console.log(this.$data);
     },
     // password 중복체크
-    onCheck() {
-
-    }
+    onCheck() {},
   },
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -120,13 +104,18 @@ div {
 
 .white-bg {
   background-color: white;
+  /* width: 60%; */
   border-radius: 8px;
   padding: 20px;
 }
 
-.login{
+.login {
   position: static;
   height: 100%;
+}
+
+.vBtn {
+  margin: 20px;
 }
 
 ul {
